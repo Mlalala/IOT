accd40b263827d04f3d1966a73e48ce784c756af
lamontagnette <martinez.simon@outlook.fr>
2018-10-03 14:33:31 +0200

    sfdg

--- utils_knx.py
+++ utils_knx.py
@@ -3,8 +3,11 @@ import socket, sys
 
 def write(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_size) :
   # /!\ ports can be different
-  data_endpoint = (ip_source, port_cli) #"Connection_Request","Connection_State_Request","Disconnect_Request","Connection_Response","Connection_State_Response","Disconnect_Response"
-  control_endpoint = (ip_source, port_cli) #"TunnelingRequest" and "Tunneling Ack"
+  #data_endpoint = (ip_source, port_cli) #"Connection_Request","Connection_State_Request","Disconnect_Request","Connection_Response","Connection_State_Response","Disconnect_Response"
+  #control_endpoint = (ip_source, port_cli) #"TunnelingRequest" and "Tunneling Ack"
+
+  data_endpoint = ('0.0.0.0',port_cli)
+  control_endpoint = ('0.0.0.0',port_cli) 
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   sock.bind((ip_source, port_cli))
@@ -73,11 +76,15 @@ def write(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_si
 
 def read(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_size):
     # /!\ ports can be different
-  data_endpoint = (ip_source, port_cli) #"Connection_Request","Connection_State_Request","Disconnect_Request","Connection_Response","Connection_State_Response","Disconnect_Response"
-  control_endpoint = (ip_source, port_cli) #"TunnelingRequest" and "Tunneling Ack"
+  #data_endpoint = (ip_sources, port_cli) #"Connection_Request","Connection_State_Request","Disconnect_Request","Connection_Response","Connection_State_Response","Disconnect_Response"
+  #control_endpoint = (ip_source, port_cli) #"TunnelingRequest" and "Tunneling Ack"
+
+  data_endpoint = ('0.0.0.0',0)
+  control_endpoint = ('0.0.0.0',0)
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind((ip_source, port_cli))
+  #sock.bind((ip_source, port_cli))
+  sock.bind(('', port_cli))
 
   # -> Connection Resquest
   conn_req_object = knxnet.create_frame(knxnet.ServiceTypeDescriptor.CONNECTION_REQUEST,control_endpoint,data_endpoint)
@@ -109,12 +116,14 @@ def read(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_siz
   data_recv, addr = sock.recvfrom(1024)
   tunn_resp_object = knxnet.decode_frame(data_recv)
 
-  # <- Tunnelling Request
+  # <- Tunnelling Request Confirm
   data_recv, addr = sock.recvfrom(1024)
   tunn_req_object_res = knxnet.decode_frame(data_recv)
+  tunn_channel_id = tunn_req_object_res.channel_id
+  tunn_sequence_counter = tunn_req_object_res.sequence_counter
 
   # -> Tunnelling Ack
-  tunn_ack_object = knxnet.create_frame(knxnet.ServiceTypeDescriptor.TUNNELLING_ACK,conn_channel_id,conn_status)
+  tunn_ack_object = knxnet.create_frame(knxnet.ServiceTypeDescriptor.TUNNELLING_ACK,tunn_channel_id,0,tunn_sequence_counter)
   tunn_ack_dtgrm = tunn_req_object.frame
   sock.sendto (tunn_ack_dtgrm, (ip_dest, port_gate))
 
@@ -122,8 +131,6 @@ def read(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_siz
   data_recv, addr = sock.recvfrom(1024)
   tunn_req_object_res2 = knxnet.decode_frame(data_recv)
 
-
-  """
   # -> Disconnect Request
   dis_req_object = knxnet.create_frame(knxnet.ServiceTypeDescriptor.DISCONNECT_REQUEST,conn_channel_id,control_endpoint)
   dis_req_dtgrm = tunn_req_object.frame
@@ -133,7 +140,7 @@ def read(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_siz
   data_recv, addr = sock.recvfrom(1024)
   dis_resp_object = knxnet.decode_frame(data_recv)
   #dis_status = conn_resp_stat_object.status
-  """
+  
 
   #debug
   print("READ")
@@ -146,7 +153,8 @@ def read(ip_source, ip_dest, port_cli, port_gate, dest_addr_group,data, data_siz
   print("###tunn_res_object_res\n",tunn_req_object_res)
   print("###tunn_ack_object\n",tunn_ack_object) 
   print("###tunn_req_object_res2\n",tunn_req_object_res2)
+  #print("###tunn_req_object_res2\n",tunn_req_object_res3)
   
 
-  #print("###dis_req_object\n",dis_req_object)
-  #print("###dis_resp_object\n",dis_resp_object)
+  print("###dis_req_object\n",dis_req_object)
+  print("###dis_resp_object\n",dis_resp_object)
